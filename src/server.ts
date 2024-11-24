bc24c534d424004f5abcaa675aceae6e6419ab78
luanrobert07 <luan.robert@gec.inatel.br>
2024-11-24 16:45:33 -0300

    feat: porta aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import { app } from './app'
 import { env } from './env'
 
-app
-  .listen({
-    host: '127.0.0.1',
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log('HTTP Server Running!')
-  })
+app.listen({
+  host: '127.0.0.1',
+  port: env.PORT || 3000,
+})
+.then(() => {
+  console.log('HTTP Server Running!');
+})
+.catch(err => {
+  console.error('Error starting server:', err);
+});
+
